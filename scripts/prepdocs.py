9fd4b44c9971d4c1208890a921d7ca38724f9b50
huqianghui <huqianghui0613@163.com>
2023-06-10 08:24:31 +0800

    add files

---
+++ scripts/prepdocs.py
@@ -0,0 +1,328 @@
+from datetime import datetime
+import os
+import argparse
+import glob
+import html
+import io
+import re
+import time
+import openai
+from pypdf import PdfReader, PdfWriter
+from azure.identity import AzureDeveloperCliCredential
+from azure.core.credentials import AzureKeyCredential
+from azure.storage.blob import BlobServiceClient
+from azure.search.documents.indexes import SearchIndexClient
+from azure.search.documents.indexes.models import *
+from azure.search.documents import SearchClient
+from azure.ai.formrecognizer import DocumentAnalysisClient
+
+MAX_SECTION_LENGTH = 8000
+SENTENCE_SEARCH_LIMIT = 0
+SECTION_OVERLAP = 0
+
+parser = argparse.ArgumentParser(
+    description="Prepare documents by extracting content from PDFs, splitting content into sections, uploading to blob storage, and indexing in a search index.",
+    epilog="Example: prepdocs.py '..\data\*' --storageaccount myaccount --container mycontainer --searchservice mysearch --index myindex -v"
+    )
+parser.add_argument("--directory", help="Files of the directory to be processed",default='/Users/huqianghui/Downloads/1.eastmoney-workspace/consultation/small-pdf-demo-test-script/testdata202303_1/')
+parser.add_argument("--category", help="Value for the category field in the search index for all sections indexed in this run",default="consultation-test-small-file")
+parser.add_argument("--skipblobs", action="store_true", help="Skip uploading individual pages to Azure Blob Storage")
+parser.add_argument("--storageaccount", help="Azure Blob Storage account name",default="pdftestsa")
+parser.add_argument("--container", help="Azure Blob Storage container name",default="consultation4")
+parser.add_argument("--storagekey", required=False, help="Optional. Use this Azure Blob Storage account key instead of the current user identity to login (use az login to set current user for Azure)",default="aeQIqOpQw0+IW5cAfMqCfeNJ1Fd6EKXKuSqZJZNDpia4z+F2XnyLhRFtyFvuosykaqWvLwLiEOIT+AStIarPJQ==")
+parser.add_argument("--tenantid", required=False, help="Optional. Use this to define the Azure directory where to authenticate)")
+parser.add_argument("--searchservice", help="Name of the Azure Cognitive Search service where content should be indexed (must exist already)",default="gptkb-odk2jh5qispeo")
+parser.add_argument("--index", help="Name of the Azure Cognitive Search index where content should be indexed (will be created if it doesn't exist)",default="consultation4")
+parser.add_argument("--searchkey", required=False, help="Optional. Use this Azure Cognitive Search account key instead of the current user identity to login (use az login to set current user for Azure)")
+parser.add_argument("--remove", action="store_true", help="Remove references to this document from blob storage and the search index")
+parser.add_argument("--removeall", action="store_true", help="Remove all blobs from blob storage and documents from the search index")
+parser.add_argument("--localpdfparser", action="store_true", help="Use PyPdf local PDF parser (supports only digital PDFs) instead of Azure Form Recognizer service to extract text, tables and layout from the documents")
+parser.add_argument("--formrecognizerservice", required=False, help="Optional. Name of the Azure Form Recognizer service which will be used to extract text, tables and layout from the documents (must exist already)",default="cog-fr-odk2jh5qispeo")
+parser.add_argument("--formrecognizerkey", required=False, help="Optional. Use this Azure Form Recognizer account key instead of the current user identity to login (use az login to set current user for Azure)",default="c201a6b2ac7c4949a14505b7f28af22b")
+parser.add_argument("--verbose", "-v", action="store_true", help="Verbose output",default=True)
+args = parser.parse_args()
+
+# Use the current user identity to connect to Azure services unless a key is explicitly set for any of them
+azd_credential = AzureDeveloperCliCredential() if args.tenantid == None else AzureDeveloperCliCredential(tenant_id=args.tenantid)
+default_creds = azd_credential if args.searchkey == None or args.storagekey == None else None
+search_creds = default_creds if args.searchkey == None else AzureKeyCredential(args.searchkey)
+if not args.skipblobs:
+    storage_creds = default_creds if args.storagekey == None else args.storagekey
+if not args.localpdfparser:
+    # check if Azure Form Recognizer credentials are provided
+    if args.formrecognizerservice == None:
+        print("Error: Azure Form Recognizer service is not provided. Please provide formrecognizerservice or use --localpdfparser for local pypdf parser.")
+        exit(1)
+    formrecognizer_creds = default_creds if args.formrecognizerkey == None else AzureKeyCredential(args.formrecognizerkey)
+
+def blob_name_from_file_page(filename, page = 0):
+    if os.path.splitext(filename)[1].lower() == ".pdf":
+        return os.path.splitext(os.path.basename(filename))[0] + f"-{page}" + ".pdf"
+    else:
+        return os.path.basename(filename)
+
+def upload_blobs(filename):
+    blob_service = BlobServiceClient(account_url=f"https://{args.storageaccount}.blob.core.windows.net", credential=storage_creds)
+    blob_container = blob_service.get_container_client(args.container)
+    if not blob_container.exists():
+        blob_container.create_container()
+
+    # upload the original file because the pdf files are small
+    with open(filename,"rb") as data:
+            file_name = os.path.basename(filename)
+            blob_container.upload_blob(file_name, data, overwrite=True)
+
+def remove_blobs(filename):
+    if args.verbose: print(f"Removing blobs for '{filename or '<all>'}'")
+    blob_service = BlobServiceClient(account_url=f"https://{args.storageaccount}.blob.core.windows.net", credential=storage_creds)
+    blob_container = blob_service.get_container_client(args.container)
+    if blob_container.exists():
+        if filename == None:
+            blobs = blob_container.list_blob_names()
+        else:
+            prefix = os.path.splitext(os.path.basename(filename))[0]
+            blobs = filter(lambda b: re.match(f"{prefix}.pdf", b), blob_container.list_blob_names(name_starts_with=os.path.splitext(os.path.basename(prefix))[0]))
+        for b in blobs:
+            if args.verbose: print(f"\tRemoving blob {b}")
+            blob_container.delete_blob(b)
+
+def table_to_html(table):
+    table_html = "<table>"
+    rows = [sorted([cell for cell in table.cells if cell.row_index == i], key=lambda cell: cell.column_index) for i in range(table.row_count)]
+    for row_cells in rows:
+        table_html += "<tr>"
+        for cell in row_cells:
+            tag = "th" if (cell.kind == "columnHeader" or cell.kind == "rowHeader") else "td"
+            cell_spans = ""
+            if cell.column_span > 1: cell_spans += f" colSpan={cell.column_span}"
+            if cell.row_span > 1: cell_spans += f" rowSpan={cell.row_span}"
+            table_html += f"<{tag}{cell_spans}>{html.escape(cell.content)}</{tag}>"
+        table_html +="</tr>"
+    table_html += "</table>"
+    print("table_html: " + table_html)
+    return table_html
+
+def get_document_text(filename):
+    offset = 0
+    page_map = []
+    if args.localpdfparser:
+        reader = PdfReader(filename)
+        pages = reader.pages
+        for page_num, p in enumerate(pages):
+            page_text = p.extract_text()
+            page_map.append((page_num, offset, page_text))
+            offset += len(page_text)
+    else:
+        if args.verbose: print(f"Extracting text from '{filename}' using Azure Form Recognizer")
+        form_recognizer_client = DocumentAnalysisClient(endpoint=f"https://{args.formrecognizerservice}.cognitiveservices.azure.com/", credential=formrecognizer_creds, headers={"x-ms-useragent": "azure-search-chat-demo/1.0.0"})
+        with open(filename, "rb") as f:
+            poller = form_recognizer_client.begin_analyze_document("prebuilt-layout", document = f)
+        form_recognizer_results = poller.result()
+
+        for page_num, page in enumerate(form_recognizer_results.pages):
+            tables_on_page = [table for table in form_recognizer_results.tables if table.bounding_regions[0].page_number == page_num + 1]
+
+            # mark all positions of the table spans in the page
+            page_offset = page.spans[0].offset
+            page_length = page.spans[0].length
+            table_chars = [-1]*page_length
+            for table_id, table in enumerate(tables_on_page):
+                for span in table.spans:
+                    # replace all table spans with "table_id" in table_chars array
+                    for i in range(span.length):
+                        idx = span.offset - page_offset + i
+                        if idx >=0 and idx < page_length:
+                            table_chars[idx] = table_id
+
+            # build page text by replacing charcters in table spans with table html
+            page_text = ""
+            added_tables = set()
+            for idx, table_id in enumerate(table_chars):
+                if table_id == -1:
+                    page_text += form_recognizer_results.content[page_offset + idx]
+                elif not table_id in added_tables:
+                    page_text += (table_to_html(tables_on_page[table_id]))
+                    added_tables.add(table_id)
+
+            prefix = "东财财经新闻网于" + filename_date_abstract(filename) + "报道：\n"
+            page_text = page_text.replace("\n", "")
+            page_text = prefix + page_text
+
+    #print("page_text: " + page_text)
+    
+    return page_text
+
+
+openai.api_type = "azure"
+openai.api_key = "b3813d19476f464982ae9d6b6022c0ac"
+openai.api_base = "https://openai-demo3-east-us.openai.azure.com/"
+openai.api_version = "2023-03-15-preview"
+
+
+def split_text(page_map):
+    SENTENCE_ENDINGS = ["。", "!", "?"]
+    WORDS_BREAKS = [",", ";", ":", " ", "(", ")", "[", "]", "{", "}", "\t", "\n"]
+    if args.verbose: print(f"Splitting '{filename}' into sections")
+
+    def find_page(offset):
+        l = len(page_map)
+        for i in range(l - 1):
+            if offset >= page_map[i][1] and offset < page_map[i + 1][1]:
+                return i
+        return l - 1
+
+    all_text = "".join(p[2] for p in page_map)
+    length = len(all_text)
+    start = 0
+    end = length
+    while start + SECTION_OVERLAP < length:
+        last_word = -1
+        end = start + MAX_SECTION_LENGTH
+
+        if end > length:
+            end = length
+        else:
+            # Try to find the end of the sentence
+            while end < length and (end - start - MAX_SECTION_LENGTH) < SENTENCE_SEARCH_LIMIT and all_text[end] not in SENTENCE_ENDINGS:
+                if all_text[end] in WORDS_BREAKS:
+                    last_word = end
+                end += 1
+            if end < length and all_text[end] not in SENTENCE_ENDINGS and last_word > 0:
+                end = last_word # Fall back to at least keeping a whole word
+        if end < length:
+            end += 1
+
+        # Try to find the start of the sentence or at least a whole word boundary
+        last_word = -1
+        while start > 0 and start > end - MAX_SECTION_LENGTH - 2 * SENTENCE_SEARCH_LIMIT and all_text[start] not in SENTENCE_ENDINGS:
+            if all_text[start] in WORDS_BREAKS:
+                last_word = start
+            start -= 1
+        if all_text[start] not in SENTENCE_ENDINGS and last_word > 0:
+            start = last_word
+        if start > 0:
+            start += 1
+
+        section_text = all_text[start:end]
+        yield (section_text, find_page(start))
+
+        last_table_start = section_text.rfind("<table")
+        if (last_table_start > 2 * SENTENCE_SEARCH_LIMIT and last_table_start > section_text.rfind("</table")):
+            # If the section ends with an unclosed table, we need to start the next section with the table.
+            # If table starts inside SENTENCE_SEARCH_LIMIT, we ignore it, as that will cause an infinite loop for tables longer than MAX_SECTION_LENGTH
+            # If last table starts inside SECTION_OVERLAP, keep overlapping
+            if args.verbose: print(f"Section ends with unclosed table, starting next section with the table at page {find_page(start)} offset {start} table start {last_table_start}")
+            start = min(end - SECTION_OVERLAP, start + last_table_start)
+        else:
+            start = end - SECTION_OVERLAP
+        
+    if start + SECTION_OVERLAP < end:
+        yield (all_text[start:end], find_page(start))
+
+
+def create_document(filename, page_text):
+    return {
+            "id": re.sub("[^0-9a-zA-Z_-]","_",f"{filename}"),
+            "content": page_text,
+            "category": args.category,
+            "sourcefile": filename
+        }
+        
+
+def create_search_index():
+    if args.verbose: print(f"Ensuring search index {args.index} exists")
+    search_creds = AzureKeyCredential("MMiyAYkKvcBIJwY0KeQCfHzYoX0NHNvSDGFztijckJAzSeBw28QL")
+    index_client = SearchIndexClient(endpoint=f"https://{args.searchservice}.search.windows.net/",
+                                     credential=search_creds)
+    if args.index not in index_client.list_index_names():
+        index = SearchIndex(
+            name=args.index,
+            fields=[
+                SimpleField(name="id", type="Edm.String", key=True),
+                SearchableField(name="content", type="Edm.String", analyzer_name="zh-Hans.microsoft"),
+                SimpleField(name="category", type="Edm.String", filterable=True, facetable=True),
+                SimpleField(name="sourcefile", type="Edm.String", filterable=True, facetable=True)
+            ],
+            semantic_settings=SemanticSettings(
+                configurations=[SemanticConfiguration(
+                    name='default',
+                    prioritized_fields=PrioritizedFields(
+                        title_field=None, prioritized_content_fields=[SemanticField(field_name='content')]))])
+        )
+        if args.verbose: print(f"Creating {args.index} search index")
+        index_client.create_index(index)
+    else:
+        if args.verbose: print(f"Search index {args.index} already exists")
+
+def index_document(filename, document):
+    if args.verbose: print(f"Indexing document from '{filename}' into search index '{args.index}'")
+    search_client = SearchClient(endpoint=f"https://{args.searchservice}.search.windows.net/",
+                                    index_name=args.index,
+                                    credential=search_creds)
+    i = 0
+    batch = []
+    batch.append(document)
+    results = search_client.upload_documents(documents=batch)
+    succeeded = sum([1 for r in results if r.succeeded])
+    if args.verbose: print(f"\tIndexed {len(results)} document, {succeeded} succeeded")
+
+def remove_from_index(filename):
+    if args.verbose: print(f"Removing sections from '{filename or '<all>'}' from search index '{args.index}'")
+    search_client = SearchClient(endpoint=f"https://{args.searchservice}.search.windows.net/",
+                                    index_name=args.index,
+                                    credential=search_creds)
+    while True:
+        filter = None if filename == None else f"sourcefile eq '{os.path.basename(filename)}'"
+        r = search_client.search("", filter=filter, top=1000, include_total_count=True)
+        if r.get_count() == 0:
+            break
+        r = search_client.delete_documents(documents=[{ "id": d["id"] } for d in r])
+        if args.verbose: print(f"\tRemoved {len(r)} sections from index")
+        # It can take a few seconds for search results to reflect changes, so wait a bit
+        time.sleep(2)
+
+def filename_date_abstract(filename):
+    file_name = os.path.basename(filename)
+    dateAbstracted = file_name[0:8]
+    date = datetime.strptime(dateAbstracted, "%Y%m%d")
+    return date.strftime("%Y年%-m月%-d号")
+
+def traverse_folders(directory):
+    files = [file for file in os.listdir(directory) if os.path.isfile(os.path.join(directory, file)) and file.endswith(".pdf")]
+    for file in files:
+        filename =  os.path.join(directory, file)
+        print(f"Processing '{filename}'")
+        upload_blobs(filename)
+        page_text = get_document_text(filename)
+        document = create_document(file, page_text)
+        index_document(file, document)
+        try:
+            os.remove(filename)
+        except OSError as e:
+            print("文件删除失败:", e)
+
+if args.removeall:
+    remove_blobs(None)
+    remove_from_index(None)
+else:
+    if not args.remove:
+        create_search_index()
+    
+    print(f"Processing files...")
+    for directory in glob.glob(args.directory):
+        traverse_folders(directory)
+        # if args.verbose: print(f"Processing '{filename}'")
+        # if args.remove:
+        #     remove_blobs(filename)
+        #     remove_from_index(filename)
+        # elif args.removeall:
+        #     remove_blobs(None)
+        #     remove_from_index(None)
+        # else:
+        #     traverse_folders(directory)
+            # if not args.skipblobs:
+            #     upload_blobs(filename)
+            # page_map = get_document_text(filename)
+            # sections = create_sections(os.path.basename(filename), page_map)
+            # index_sections(os.path.basename(filename), sections)
+
